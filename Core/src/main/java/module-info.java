2ea975b074885096226ba7184ef5519776edd806
cgrevy <chgre23@student.sdu.dk>
2025-03-05 11:02:24 +0100

    bare bones

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module Core {
+    requires Common;
+    //requires CommonBullet;
+    requires javafx.graphics;    
+    opens dk.sdu.cbse.main to javafx.graphics;
+    uses dk.sdu.cbse.common.services.IGamePluginService;
+    uses dk.sdu.cbse.common.services.IEntityProcessingService;
+    uses dk.sdu.cbse.common.services.IPostEntityProcessingService;
+}
+
+
