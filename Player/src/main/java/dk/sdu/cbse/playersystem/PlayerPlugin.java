0589e4679d602ab63887a53129c2962662ca6b79
cgrevy <chgre23@student.sdu.dk>
2025-05-30 08:47:20 +0200

    Moved the player model to CommonPlayer module

--- Player/src/main/java/dk/sdu/cbse/playersystem/PlayerPlugin.java
+++ Player/src/main/java/dk/sdu/cbse/playersystem/PlayerPlugin.java
@@ -3,6 +3,7 @@ package dk.sdu.cbse.playersystem;
 import dk.sdu.cbse.common.data.Entity;
 import dk.sdu.cbse.common.data.GameData;
 import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.player.Player;
 import dk.sdu.cbse.common.services.IGamePluginService;
 
 public class PlayerPlugin implements IGamePluginService {
@@ -14,14 +15,12 @@ public class PlayerPlugin implements IGamePluginService {
 
     @Override
     public void start(GameData gameData, World world) {
-
         // Add entities to the world
         player = createPlayerShip(gameData);
         world.addEntity(player);
     }
 
     private Entity createPlayerShip(GameData gameData) {
-
         Entity playerShip = new Player();
         playerShip.setHealth(10);
         playerShip.setPolygonCoordinates(-5,-5,10,0,-5,5);
--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -3,6 +3,7 @@ import dk.sdu.cbse.common.services.IGamePluginService;
 module Player {
     requires Common;
     requires CommonBullet;
+    requires CommonPlayer;
     uses dk.sdu.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.cbse.playersystem.PlayerControlSystem;
